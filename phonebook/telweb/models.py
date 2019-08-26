64a85c27090fa49d0a9d1db30b5d2f84f957acbc
Sergeyklimets <sergeyklimets@gmail.com>
2019-08-26 17:01:52 +0300

    add model

--- phonebook/telweb/models.py
+++ phonebook/telweb/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Person(models.Model):
+    name = models.CharField(max_length=25)
+    surname = models.CharField(max_length=25)
+    patronymic = models.CharField(max_length=25)
+    in_tel_num = models.CharField(max_length=5)
+
+
